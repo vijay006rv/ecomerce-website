2b7113616c53f2e86707f876c900a1556074e7f6
vijay006rv <vijay006rv@gmail.com>
2025-01-21 10:02:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+var sidenav=document.querySelector(".side-navbar")
+var shopnow=document.querySelector(".header-button")
+
+
+function showNavbar(){
+  sidenav.style.left="0%"
+}
+
+function cancelNavbar(){
+     sidenav.style.left="-60%"
+}
+
+function goto(){
+  window.location.href="https://www.meesho.com/dresses-women/pl/3j3"
+}
